d7ac2731d35f4246fb4fe2ae0d2255082582e5da
luisescobar89 <luis.scobr@gmail.com>
2020-09-13 22:16:43 -0500

    update DynamicKey description

--- api/v1/config/dynatrace/model_condition_key.go
+++ api/v1/config/dynatrace/model_condition_key.go
@@ -13,7 +13,7 @@ package dynatrace
 type ConditionKey struct {
 	// The attribute to be used for comparision.
 	Attribute string `json:"attribute"`
-	// DynamicKey generated based on selected type
+	// Dynamic key generated based on selected type/attribute.
 	DynamicKey string `json:"dynamicKey,omitempty"`
 	// Defines the actual set of fields depending on the value. See one of the following objects:   * `PROCESS_CUSTOM_METADATA_KEY` -> CustomProcessMetadataConditionKey  * `HOST_CUSTOM_METADATA_KEY` -> CustomHostMetadataConditionKey  * `PROCESS_PREDEFINED_METADATA_KEY` -> ProcessMetadataConditionKey  * `STRING` -> StringConditionKey
 	Type string `json:"type,omitempty"`
